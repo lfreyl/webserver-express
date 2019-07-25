30b6559383dde2b68b98b1272e90cc45cd4cc34a
Cristian <cristian930714@hotmail.com>
2019-07-25 13:09:50 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+//Express HBS engine
+app.set('view engine', 'hbs');
+app.get('/', (req, res) => {
+    // res.send('Hola mundo')
+    // res.writeHead(200, { 'Content-Type': 'aplication/json' });
+    // let salida = {
+    //     nombre: 'Cristian',
+    //     edad: 26,
+    //     url: req.url
+
+    // }
+    res.render('home', {
+        nombre: 'criStian pEreZ'
+    });
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        nombre: 'Cristian'
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando el puerto ${port}`)
+})
